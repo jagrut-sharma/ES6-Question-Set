d73ae3e32f4083cffbe6e4a68ed76fcda8c8e878
Jagrut Sharma <jagrut.sharma1@gmail.com>
2023-02-27 18:52:56 +0530

    completed PS-4

--- index.js
+++ index.js
@@ -585,7 +585,7 @@ console.log(printProductDetails(product)); // Apple MacBook Air 2020 which is of
 
 const getMaxElement = function (numArr) {
   let [max] = numArr;
-  for (let i = 0; i < numArr.length; i++) {
+  for (let i = 1; i < numArr.length; i++) {
     if (numArr[i] > max) max = numArr[i];
   }
   return max;
@@ -596,3 +596,199 @@ let array2 = [4, 78, 8, 3, 6, 112, 324, 52, 555];
 console.log(getMaxElement(array)); // 78
 console.log(getMaxElement(array2)); // 555
 */
+
+/*
+// 2. Write an ES6 function that takes an array of numbers and returns the average of all the numbers. Avoid using in-built methods.
+
+const calculateAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(calculateAverage([1, 2, 3, 4, 5])); // 3
+*/
+
+/*
+// 3. write an ES6 function that takes an array of numbers and converts even numbers to odd numbers by adding 1 to that number.
+
+const convertEvenToOdd = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) arr[i] += 1;
+  }
+  return arr;
+};
+
+var numArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(convertEvenToOdd(numArr));
+console.log(convertEvenToOdd([2, 2, 3, 4, 6, 5]));
+// [1, 3, 3, 5, 5, 7, 7, 9, 9]
+*/
+
+/*
+// 4. write an ES6 function that takes an array of words and returns an array with all its elements whose length is greater than 5.
+
+const filterWords = function (arr) {
+  const biggerWords = [];
+  let index = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > 5) {
+      biggerWords[index] = arr[i];
+      index++;
+    }
+  }
+  return biggerWords;
+};
+
+var words = ["eat", "sleep", "code", "repeat", "neog", "community"];
+console.log(filterWords(words)); // ["repeat", "community"]
+*/
+
+/*
+// 5. Write an ES6 function that takes an array of strings and returns a new array with each string capitalized.
+
+const capitalizeWords = function (arr) {
+  const capitalized = [];
+  for (let i = 0; i < arr.length; i++) {
+    capitalized[i] = arr[i].toUpperCase();
+  }
+  return capitalized;
+};
+
+console.log(capitalizeWords(["eat", "sleep", "code", "repeat"]));
+// ["EAT", "SLEEP", "CODE", "REPEAT"]
+*/
+
+/*
+// 6. Write an ES6 function that takes an array of objects and a property name and returns a new array with only the values of that property. Avoid using in-built methods.
+
+function getValues(arr, prop) {
+  const valuesOfProp = [];
+  for (let i = 0; i < arr.length; i++) {
+    // console.log(arr[i][prop]);
+    const { [prop]: key } = arr[i];
+    valuesOfProp[i] = key;
+  }
+  return valuesOfProp;
+}
+
+console.log(
+  getValues(
+    [
+      { name: "John", age: 21 },
+      { name: "Mary", age: 22 },
+      { name: "Peter", age: 23 },
+    ],
+    "name"
+  )
+); // ["John", "Mary", "Peter"]
+*/
+
+/*
+// 7. Write an ES6 function that takes the users' details and returns the data with team ID. Avoid using in-built methods.
+
+const randomGenerator = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
+
+const idGenerated = {};
+
+const podAndTeamAllocation = function (user) {
+  const userDetails = { ...user };
+  const min = 10000;
+  const max = 999999;
+  let randomNum = randomGenerator(min, max);
+  while (idGenerated[randomGenerator] !== undefined) {
+    randomNum = randomGenerator(min, max);
+  }
+  idGenerated[randomNum] = userDetails.name;
+  userDetails.teamID = randomNum;
+  return userDetails;
+};
+
+const userData = { firstName: "John", lastName: "Dee" };
+console.log(podAndTeamAllocation(userData));
+// {firstName: 'John', lastName: 'Dee', teamId: 667543}
+*/
+
+/*
+// 8. Write an ES6 function which checks if a student already has a team. If team is not given then add them to team “A” and return the object else do nothing and return the same object. Avoid using in-built methods.
+
+const checkForTeam = function (details) {
+  const studentDetails = { ...details };
+  const { team } = studentDetails;
+  if (team === undefined) studentDetails.team = "A";
+  return studentDetails;
+};
+
+console.log(checkForTeam({ firstName: "Penn", lastName: "Ma" }));
+// {firstName: 'Penn', lastName: 'Ma', team: A}
+
+console.log(checkForTeam({ firstName: "John", lastName: "Dee", team: "B" }));
+// {firstName: 'John', lastName: 'Dee', team: B}
+
+console.log(checkForTeam({ firstName: "Priya", lastName: "Raj" }));
+// {firstName: 'Priya', lastName: 'Raj', team: A}
+*/
+
+/*
+// 9. Destructure the following code to get the desired outputs. Avoid using in-built methods.
+
+const book = {
+  title: "JavaScript: The Definitive Guide",
+  authors: [
+    { name: "David Flanagan", age: 49 },
+    { name: "Yukihiro Matsumoto", age: 57 },
+  ],
+  publisher: { name: "O'Reilly Media", location: "CA" },
+};
+
+const {
+  title,
+  // authors: [personOne, personTwo]
+  authors: [{ name: author1 }, { name: author2 = "Jagrut" }],
+  publisher: { name: publisherName },
+} = book;
+
+// const { name: author1 } = personOne;
+// const { name: author2 } = personTwo;
+
+console.log(title); // JavaScript: The Definitive Guide
+console.log(author1); // David Flanagan
+console.log(author2); // Yukihiro Matsumoto
+console.log(publisherName); // O'Reilly Media
+*/
+
+// 10. Write an ES6 function that takes an array of objects and returns the sum of all ages.
+
+const sumOfAges = function (arr) {
+  const personDetailsArr = [...arr];
+  let sum = 0;
+  for (const { age } of personDetailsArr) {
+    sum += age;
+  }
+  return sum;
+};
+
+var array = [
+  {
+    name: "Jay",
+    age: 60,
+  },
+  {
+    name: "Gloria",
+    age: 36,
+  },
+  {
+    name: "Manny",
+    age: 16,
+  },
+  {
+    name: "Joe",
+    age: 9,
+  },
+];
+
+console.log(sumOfAges(array)); // 121
